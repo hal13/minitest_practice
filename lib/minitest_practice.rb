a10f6b59af3d50dc7ec41cfbbfc2f7d3c5fe3fd8
hal13 <vagrant@enpit>
2016-07-23 09:07:45 +0000

    Second Commit

--- lib/minitest_practice.rb
+++ lib/minitest_practice.rb
@@ -2,4 +2,17 @@ require "minitest_practice/version"
 
 module MinitestPractice
   # Your code goes here...
+  class Main
+    def fizz_buzz(n)
+      if (n % 3) == 0 && (n % 5) == 0
+        return 'FizzBuzz'
+      elsif (n % 3) == 0
+        return 'Fizz'
+      elsif (n % 5) == 0
+        return 'Buzz'
+      else
+        return ''
+      end
+    end
+  end
 end
--- test/minitest_practice_test.rb
+++ test/minitest_practice_test.rb
@@ -1,8 +1,35 @@
 require 'test_helper'
 
 class MinitestPracticeTest < Minitest::Test
+  def setup
+    @main = ::MinitestPractice::Main.new
+  end
+
   def test_that_it_has_a_version_number
     refute_nil ::MinitestPractice::VERSION
+  end
+
+  def test_fizz_buzz
+    assert_equal '', @main.fizz_buzz(1), '1'
+    assert_equal '', @main.fizz_buzz(2), '2'
+    assert_equal 'Fizz', @main.fizz_buzz(3), '3'
+    assert_equal '', @main.fizz_buzz(4), '4'
+    assert_equal 'Buzz', @main.fizz_buzz(5), '5'
+    assert_equal 'Fizz', @main.fizz_buzz(6), '6'
+    assert_equal '', @main.fizz_buzz(7), '7'
+    assert_equal '', @main.fizz_buzz(8), '8'
+    assert_equal 'Fizz', @main.fizz_buzz(9), '9'
+    assert_equal 'Buzz', @main.fizz_buzz(10), '10'
+    assert_equal '', @main.fizz_buzz(11), '11'
+    assert_equal 'Fizz', @main.fizz_buzz(12), '12'
+    assert_equal '', @main.fizz_buzz(13), '13'
+    assert_equal '', @main.fizz_buzz(14), '14'
+    assert_equal 'FizzBuzz', @main.fizz_buzz(15), '15'
+    assert_equal '', @main.fizz_buzz(16), '16'
+    assert_equal '', @main.fizz_buzz(17), '17'
+    assert_equal 'Fizz', @main.fizz_buzz(18), '18'
+    assert_equal '', @main.fizz_buzz(19), '19'
+    assert_equal 'Buzz', @main.fizz_buzz(20), '20'
   end
 
   def test_it_does_something_useful
